9ea5f3493a5925a9b3ead80c91e15b5c16c2da67
hulang <1013644379@qq.com>
2026-07-09 21:25:12 +0800

    去掉nvim-treesitter失效缩进indentexpr；增加一些命令

---
+++ nvim/lua/config/cmds.lua
@@ -0,0 +1,29 @@
+local function do_copy(content)
+  vim.fn.setreg("+", content)
+  if content ~= "" then
+    vim.notify("Copied: " .. content, vim.log.levels.INFO)
+  else
+    vim.notify("复制失败", vim.log.levels.ERROR)
+  end
+end
+
+vim.api.nvim_create_user_command("CopyAbsolutePath", function()
+  local path = vim.fs.normalize(vim.fn.expand("%:p"))
+  do_copy(path)
+end, { desc = "复制绝对路径" })
+
+vim.api.nvim_create_user_command("CopyRootPath", function()
+  local _, misc = pcall(require, "mini.misc")
+  local path = misc and misc.find_root(0) or ""
+  if path then
+    do_copy(path)
+    return
+  else
+    vim.notify("未找到根路径", vim.log.levels.WARN)
+  end
+end, { desc = "复制根路径" })
+
+vim.api.nvim_create_user_command("CopyFilename", function()
+  local filename = vim.fn.expand("%:t")
+  do_copy(filename)
+end, { desc = "复制文件名" })
